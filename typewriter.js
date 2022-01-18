fad5e6b28fdd1abf7e8f6082df0952bab18444ba
Brendan Woo <woo.brendan@gmail.com>
2022-01-18 18:12:56 +0000

    refactored to accept any string

--- typewriter.js
+++ typewriter.js
@@ -1,11 +1,12 @@
 const sentence = 'hello there from lighthouse labs';
+const otherSentence = 'This is a string'
 
-let wordArray = sentence.split('');
-const typeWriter = function() {
+const typeWriter = function(string) {
+  let wordArray = string.split('');
   for (let i = 0; i < wordArray.length; i++) {
     setTimeout(() => process.stdout.write(wordArray[i]), (i + 1) * 50);
   }
   setTimeout(() => console.log('\n'), wordArray.length * 50);
 };
 
-typeWriter();
+typeWriter(otherSentence);
