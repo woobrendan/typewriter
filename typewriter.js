e72ff0331e28b5a03af561d217ebcb3b021af774
Brendan Woo <woo.brendan@gmail.com>
2022-01-18 18:10:54 +0000

    lint doc

--- typewriter.js
+++ typewriter.js
@@ -1,11 +1,11 @@
 const sentence = 'hello there from lighthouse labs';
 
-let wordArray = sentence.split('')
-const typeWriter = function () {
+let wordArray = sentence.split('');
+const typeWriter = function() {
   for (let i = 0; i < wordArray.length; i++) {
-    setTimeout(() => process.stdout.write(wordArray[i]), (i + 1) * 50 )
+    setTimeout(() => process.stdout.write(wordArray[i]), (i + 1) * 50);
   }
   setTimeout(() => console.log('\n'), wordArray.length * 50);
-}
+};
 
 typeWriter();
